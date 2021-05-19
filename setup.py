9d4b234a79b7469cf5cc547a685f658a6422588b
Fredrik Borg <fredrikb@mnemonic.no>
2021-05-19 08:50:42 +0200

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), "rb") as f:
 
 setup(
     name="aep",
-    version="0.1.2",
+    version="0.1.3",
     author="mnemonic AS",
     zip_safe=True,
     author_email="opensource@mnemonic.no",
